c2dd52ead884032b6290c6f5f412a34f985fe65f
karengomezs <karengomezs0601@gmail.com>
2023-05-18 17:22:40 -0500

    delete navbar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Navbar from "@/components/navbar";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Navbar />
+
       <body>
         <Main />
         <NextScript />
