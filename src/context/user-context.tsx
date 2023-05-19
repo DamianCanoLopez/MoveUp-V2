165a52186a6ce4e2d6a8dc4edf05d3f0841a7ce0
karengomezs <karengomezs0601@gmail.com>
2023-05-18 20:17:38 -0500

    context user types

---
+++ src/context/user-context.tsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+  lastName: string;
+  email: string;
+  city: string;
+  token: string;
+  role: string;
+}
+
+const UserContext = createContext<any>(undefined);
+
+export const UserProvider = (props: any) => {
+  const [user, setUser] = useState<User | undefined>();
+
+  function logOut(): void {
+    setUser();
+  }
+  return (
+    <UserContext.Provider value={{ user, setUser, logOut }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
